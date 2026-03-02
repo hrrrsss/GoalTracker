28d550ca4f2284e691f565c17974e27c84f95321
hrrrsss <danil_gusev2004@bk.ru>
2026-03-02 12:20:49 +0300

    Init

---
+++ main.py
@@ -0,0 +1,8 @@
+class Goal:
+    """Goal high level"""
+
+    def __init__(self, name, deadline, tasks):
+        self.name = name
+        self.deadline = deadline
+        self.tasks = tasks
+    
